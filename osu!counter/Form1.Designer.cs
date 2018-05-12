fe708f5c3049730ed9d70848ee8767626ea26933
sgfic <sgfic@DDHELLO>
2018-05-12 13:16:26 +0800

    毫无卵用！

---
+++ osu!counter/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace osu_counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Osu!运行状态：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(107, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "未运行";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(13, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 76);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "计数区域";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(148, 27);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "手动保存";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "X：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Z：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(12, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 76);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "设置区域";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 22);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "保存";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 22);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "修改";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(51, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 21);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "X";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(10, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 12);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "KEY2:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(51, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(60, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Z";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 12);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "KEY1:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(161, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "清空计数";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(219, 194);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(45, 27);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "关于";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 221);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Osu!Counter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ osu!counter/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace osu_counter
+{
+    public partial class Form1 : Form
+    {
+        public Keys keyo = Keys.Z;
+        public Keys keyt = Keys.X;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        KeyboardHook kh;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label5.Text = ConfigurationManager.AppSettings["key1count"];
+            label6.Text = ConfigurationManager.AppSettings["key2count"];
+            
+            textBox1.Text = ConfigurationManager.AppSettings["key1"];
+            textBox2.Text = ConfigurationManager.AppSettings["key2"];
+            keyo = (Keys)Enum.Parse(typeof(Keys), textBox1.Text);
+            keyt = (Keys)Enum.Parse(typeof(Keys), textBox2.Text);
+        }
+        void zhuce()
+        {
+            kh = new KeyboardHook();
+            kh.SetHook();
+            kh.OnKeyDownEvent += ondown;
+        }
+        void unzhuce()
+        {
+            kh.UnHook();
+        }
+        void ondown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == (keyo))
+            {
+                int z = int.Parse(label5.Text);
+                z += 1;
+                label5.Text = z.ToString();
+            }
+            if (e.KeyData == (keyt))
+            {
+                int x = int.Parse(label6.Text);
+                x += 1;
+                label6.Text = x.ToString();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult mo = MessageBox.Show(null, "若执行此操作，您的所有记录将被清空，届时将无法恢复\n不过您仍可以通过手动设置来修改回来", "您确定吗？", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(mo == DialogResult.Yes)
+            {
+                //清空记录数据
+                label5.Text = "0";
+                label6.Text = "0";
+                MessageBox.Show(null, "已清空", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                //不变
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Configuration cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            cfa.AppSettings.Settings["key1count"].Value = label5.Text;
+            cfa.AppSettings.Settings["key2count"].Value = label6.Text;
+            cfa.Save();
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Configuration cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            cfa.AppSettings.Settings["key1count"].Value = label5.Text;
+            cfa.AppSettings.Settings["key2count"].Value = label6.Text;
+            cfa.Save();
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Configuration cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            cfa.AppSettings.Settings["key1"].Value = textBox1.Text;
+            cfa.AppSettings.Settings["key2"].Value = textBox2.Text;
+            cfa.Save();
+            ConfigurationManager.RefreshSection("appSettings");
+            ConfigurationManager.RefreshSection("appSettings");
+            textBox1.Text = ConfigurationManager.AppSettings["key1"];
+            textBox2.Text = ConfigurationManager.AppSettings["key2"];
+            keyo = (Keys)Enum.Parse(typeof(Keys), textBox1.Text);
+            keyt = (Keys)Enum.Parse(typeof(Keys), textBox2.Text);
+            MessageBox.Show(null, "已成功修改！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] app = Process.GetProcessesByName("osu!");
+            if (app.Length > 0)
+                {
+                    if (label2.Text == "未运行")
+                    {
+                        label2.Text = "正在运行";
+                        label2.ForeColor = Color.Green;
+                        zhuce();
+                    }
+                }
+                else
+                {
+                    if(label2.Text == "正在运行")
+                    {
+                        label2.Text = "未运行";
+                        label2.ForeColor = Color.Red;
+                    unzhuce();
+                    }
+                }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult ppp = MessageBox.Show(null, "你真是个会发现的孩子呢！\n感谢您使用osu!counter\n本程序是由ddhello用C#开发的按键记录程序！\n您如果对我们的源码感兴趣的话可以点击取消进入我们的Github查看哦！（虽然没有人）\n如果你想联系我的话可以发送email到sgfick@163.com\n此上。2018.5.12","隐藏的提醒",MessageBoxButtons.OKCancel,MessageBoxIcon.Information);
+            if(ppp == DialogResult.OK)
+            {
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
